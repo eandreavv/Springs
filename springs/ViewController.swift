53b44beb473c75116c724024d66c488654e06012
Maestro <Maestro@imac-de-maestro-10.local>
2018-01-22 18:55:02 -0700

    psi termino los cuadros

--- springs/ViewController.swift
+++ springs/ViewController.swift
@@ -15,21 +15,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var vwCuadroRosa: UIView!
     
     var azulArriba = false
+    var rosaArriba = false
+    var ocupado = false
     
     @IBAction func btnEscala1(_ sender: Any) {
     
         if azulArriba
         {
-            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: [], animations: {
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
                 
                 self.vwCuadroAzul.center.y += 100
-                self.vwCuadroAzul.center.x = 16
+                self.vwCuadroAzul.center.x = 28
                 self.vwCuadroAzul.transform = CGAffineTransform(scaleX: 1, y: 1)
                 
             }, completion: nil)
          
         }else{
-            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: [], animations: {
+            UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
                 
                 self.vwCuadroAzul.center.y -= 100
                 self.vwCuadroAzul.center.x = self.view.bounds.width / 2
@@ -45,7 +47,32 @@ class ViewController: UIViewController {
     
 
     @IBAction func btnEscala2(_ sender: Any) {
+       
+        if rosaArriba {
+        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            
+            self.vwCuadroRosa.center.y += 100
+            self.vwCuadroRosa.center.x = 267
+            self.vwCuadroRosa.transform = CGAffineTransform(scaleX: 1, y: 1)
+            
+            }, completion: nil)
+            
+        }else{
+            
+            UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+                
+                self.vwCuadroRosa.center.y -= 100
+                self.vwCuadroRosa.center.x = self.view.bounds.width / 2
+                self.vwCuadroRosa.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+                
+            }, completion: nil)
+            
+        }
+        
+        rosaArriba = !rosaArriba
     }
+    
+    
     
     
     override func viewDidLoad() {
